590edd9d9164951a7f167c6261edace40d19549b
Mauro Oliveira <mauro.anselmo.oliveira@gmail.com>
2026-05-17 14:44:35 -0300

    .

--- mauro-files/install-custom-nodes.sh
+++ mauro-files/install-custom-nodes.sh
@@ -9,17 +9,24 @@ NC='\033[0m'
 SCRIPT_DIR="$(cd -- "$(dirname -- "${BASH_SOURCE[0]}")" && pwd)"
 COMFY_ROOT="$(dirname "$SCRIPT_DIR")"
 
+clone_node() {
+    local url="$1"
+    local dir="${2:-$(basename "$url" .git)}"
+    rm -rf "$dir"
+    git clone "$url" "$dir"
+}
+
 cd "$COMFY_ROOT/custom_nodes"
-git clone https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
-git clone https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git comfyui-frame-interpolation
-git clone https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git comfyui-videohelpersuite
-git clone https://github.com/kijai/ComfyUI-WanVideoWrapper.git comfyui-wanvideowrapper
-git clone https://github.com/kijai/ComfyUI-KJNodes.git comfyui-kjnodes
-git clone https://github.com/DoctorDiffusion/ComfyUI-MediaMixer.git comfyui-mediamixer
-git clone https://github.com/city96/ComfyUI-GGUF ComfyUI-GGUF
-git clone https://github.com/welltop-cn/ComfyUI-TeaCache
-git clone https://github.com/yolain/ComfyUI-Easy-Use
-git clone https://github.com/rgthree/rgthree-comfy.git
+clone_node https://github.com/ltdrdata/ComfyUI-Manager comfyui-manager
+clone_node https://github.com/Fannovel16/ComfyUI-Frame-Interpolation.git comfyui-frame-interpolation
+clone_node https://github.com/Kosinkadink/ComfyUI-VideoHelperSuite.git comfyui-videohelpersuite
+clone_node https://github.com/kijai/ComfyUI-WanVideoWrapper.git comfyui-wanvideowrapper
+clone_node https://github.com/kijai/ComfyUI-KJNodes.git comfyui-kjnodes
+clone_node https://github.com/DoctorDiffusion/ComfyUI-MediaMixer.git comfyui-mediamixer
+clone_node https://github.com/city96/ComfyUI-GGUF ComfyUI-GGUF
+clone_node https://github.com/welltop-cn/ComfyUI-TeaCache
+clone_node https://github.com/yolain/ComfyUI-Easy-Use
+clone_node https://github.com/rgthree/rgthree-comfy.git
 
 # temporary fix from https://github.com/welltop-cn/ComfyUI-TeaCache/issues/178
 ln -sf "$SCRIPT_DIR/files/nodes.py" "$COMFY_ROOT/custom_nodes/ComfyUI-TeaCache/nodes.py"
